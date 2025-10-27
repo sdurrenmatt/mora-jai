65c758635c6ac5a16547774cc2b1e7f336a8d4aa
Steven Dürrenmatt <steven.durrenmatt@gmail.com>
2025-10-27 15:41:25 +0100

    Update tile colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,14 +1,14 @@
 import type { Color } from "../lib/puzzle/types"
 
 export const ColorHexCodes: Record<Color, string> = {
-    Black: "#1E252E",
-    Blue: "#1C71C2",
+    Black: "#1e252e",
+    Blue: "#1c71c2",
     Gray: "#687278",
-    Green: "#1A9729",
-    Orange: "#C47D2F",
-    Pink: "#D486D8",
-    Red: "#9B252D",
+    Green: "#1a9729",
+    Orange: "#c47d2F",
+    Pink: "#d486d8",
+    Red: "#9b252d",
     Violet: "#892597",
-    White: "#FFFFFF",
-    Yellow: "#B3B02F",
+    White: "#f5faff",
+    Yellow: "#b3b02f",
 }
