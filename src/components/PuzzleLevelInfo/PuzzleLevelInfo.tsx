b262ec88d9f5e3494962dc35f9330a147b01eaf9
Steven Dürrenmatt <steven.durrenmatt@gmail.com>
2025-11-07 23:38:48 +0100

    Make puzzle level info fully responsive

--- src/components/PuzzleLevelInfo/PuzzleLevelInfo.tsx
+++ src/components/PuzzleLevelInfo/PuzzleLevelInfo.tsx
@@ -7,13 +7,15 @@ export default function PuzzleLevelInfo() {
     const { currentWorld, currentLevel } = useContext(GameContext)
 
     return (
-        <div className="puzzle-level-info">
-            <div className="puzzle-level-info__world">
-                <AnimatedText text={currentWorld} />
-            </div>
+        <div className="puzzle-level-info-container">
+            <div className="puzzle-level-info">
+                <div className="puzzle-level-info__world">
+                    <AnimatedText text={currentWorld} />
+                </div>
 
-            <div className="puzzle-level-info__level">
-                <AnimatedText text={`Level ${currentLevel.name}`} />
+                <div className="puzzle-level-info__level">
+                    <AnimatedText text={`Level ${currentLevel.name}`} />
+                </div>
             </div>
         </div>
     )
