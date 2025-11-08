d3dcf2df5248c71c2ec810a3cb947a1f5d6947c4
Steven Dürrenmatt <steven.durrenmatt@gmail.com>
2025-11-08 16:20:39 +0100

    Add puzzle logo

---
+++ src/components/PuzzleLogo/PuzzleLogo.tsx
@@ -0,0 +1,12 @@
+import "./PuzzleLogo.css"
+import logo from "../../assets/images/logo.png"
+
+export default function PuzzleLogo() {
+    return (
+        <img
+            className="puzzle-logo"
+            src={logo}
+            alt="Mora Jai"
+        />
+    )
+}
