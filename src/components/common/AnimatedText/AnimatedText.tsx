8864f2636a3268c198e7b60c070086aa5010d0f4
Steven Dürrenmatt <steven.durrenmatt@gmail.com>
2025-11-11 01:39:43 +0100

    Fix animated text accessibility

--- src/components/common/AnimatedText/AnimatedText.tsx
+++ src/components/common/AnimatedText/AnimatedText.tsx
@@ -2,15 +2,16 @@ import "./AnimatedText.css"
 
 export default function AnimatedText({ text }: { text: string }) {
     return (
-        <>
+        <span className="animated-text" aria-label={text}>
             {text.split("").map((char, index) => (
                 <span
                     key={index}
                     className="animated-text__letter"
+                    aria-hidden="true"
                 >
                     {char === " " ? "\u00A0" : char}
                 </span>
             ))}
-        </>
+        </span>
     )
 }
