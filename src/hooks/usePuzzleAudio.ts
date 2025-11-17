b8e4d0e87bd9f70416e9a2b8b3e8aae7300fd69f
Steven Dürrenmatt <steven.durrenmatt@gmail.com>
2025-11-17 17:26:34 +0100

    Reduce sound volume

--- src/hooks/usePuzzleAudio.ts
+++ src/hooks/usePuzzleAudio.ts
@@ -1,13 +1,13 @@
 
 import useSound from "use-sound"
 import clickSound from "../assets/sounds/click.mp3"
-import lightSwitchSound from "../assets/sounds/light-switch.mp3"
-import openingLittleBoxSound from "../assets/sounds/opening-little-box.mp3"
+import openSound from "../assets/sounds/open.mp3"
+import switchSound from "../assets/sounds/switch.mp3"
 
 export function usePuzzleAudio() {
     const [playTileClick] = useSound(clickSound)
-    const [playCornerClick] = useSound(lightSwitchSound)
-    const [playSolved] = useSound(openingLittleBoxSound)
+    const [playCornerClick] = useSound(switchSound)
+    const [playSolved] = useSound(openSound)
 
     return { playTileClick, playCornerClick, playSolved }
 }
